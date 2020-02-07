22b724862712a51c3bc3166def66f7cf7dbd02a4
vedantsawant <vedant.sawant.2604@gmail.com>
2020-02-07 21:33:19 +0530

    error

--- app/src/main/java/com/example/csiapp1/EventFragment.java
+++ app/src/main/java/com/example/csiapp1/EventFragment.java
@@ -9,20 +9,26 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import android.R;
 
 public class EventFragment extends Fragment {
     TabLayout mytablayout;
+    FloatingActionButton addevent;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        mytablayout = (TabLayout) findViewById(R.id.mytablayout);
         return inflater.inflate(R.layout.event_fragment,container,false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setContentView(R.layout.event_fragment);
+        addevent= findv
     }
 }
