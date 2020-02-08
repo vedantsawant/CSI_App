835dcf89f7ede11c2a366a1cf4f4167ab1c06d8c
SNexusG <swapnilgore029@gmail.com>
2020-02-08 16:24:27 +0530

    Swapnil changes added

--- app/src/main/java/com/example/csiapp1/SE_council.java
+++ app/src/main/java/com/example/csiapp1/SE_council.java
@@ -1,17 +1,40 @@
 package com.example.csiapp1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
-public class SE_council extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SE_council extends AppCompatActivity implements ImageAdapter.onItemClickListener {
 
     private FloatingActionButton fb1;
+    private RecyclerView mRecyclerView;
+    private ImageAdapter mAdapter;
+    private ProgressBar mProgressCircle;
+    private FirebaseStorage mStorage;
+    private DatabaseReference databaseReference;
+    private ValueEventListener mDBlistener;
+    private List<Upload> mUploads;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +49,86 @@ public class SE_council extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mProgressCircle = findViewById(R.id.progress_circle);
+
+        mUploads = new ArrayList<>();
+
+
+        mAdapter = new ImageAdapter(SE_council.this, mUploads);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mAdapter.setItemClickListener(SE_council.this);
+
+        mStorage = FirebaseStorage.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("csi_uploads");
+
+        mDBlistener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                mUploads.clear();
+
+                for(DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    upload.setmKey(postSnapshot.getKey());
+                    mUploads.add(upload);
+                }
+
+                mAdapter.notifyDataSetChanged();
+
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(SE_council.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onItemClick(int position, String details, String name) {
+        DetailsDialog detailsDialog = new DetailsDialog(details, name);
+        detailsDialog.show(getSupportFragmentManager(), "details dialog");
+    }
+
+    @Override
+    public void onEditClick(int position) {
+        Toast.makeText(this, "Edit at position " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDeleteClick(int position) {
+        Upload selectItem = mUploads.get(position);
+        final String selectKey = selectItem.getmKey();
+
+        StorageReference imageRef = mStorage.getReferenceFromUrl(selectItem.getMimageUrl());
+        imageRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                databaseReference.child(selectKey).removeValue();
+                Toast.makeText(SE_council.this, "Item deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(SE_council.this, "Deletion Failed!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        databaseReference.removeEventListener(mDBlistener);
     }
 }
+
