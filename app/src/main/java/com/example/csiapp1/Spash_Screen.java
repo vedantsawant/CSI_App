fc6079ea47b21f1dd330c7f719bf39ebaab33a1f
SNexusG <swapnilgore029@gmail.com>
2020-03-20 14:46:11 +0530

    spash commit

---
+++ app/src/main/java/com/example/csiapp1/Spash_Screen.java
@@ -0,0 +1,39 @@
+package com.example.csiapp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class Spash_Screen extends AppCompatActivity {
+
+    private ProgressBar splash_progress_bar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spash__screen);
+
+
+        splash_progress_bar = findViewById(R.id.splash_progress);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(splash_progress_bar.getProgress() != 100){
+                    splash_progress_bar.incrementProgressBy(20);
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent intent = new Intent(Spash_Screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        t1.start();
+    }
+}
