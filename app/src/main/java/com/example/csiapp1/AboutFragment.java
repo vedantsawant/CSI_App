67b48ce252320c54c4fd2958f0ab75dc519d07b3
Vedant Sawant <vedant.sawant.2604@gmail.com>
2020-04-11 12:26:09 +0530

    almost done

--- app/src/main/java/com/example/csiapp1/AboutFragment.java
+++ app/src/main/java/com/example/csiapp1/AboutFragment.java
@@ -1,5 +1,6 @@
 package com.example.csiapp1;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import mehdi.sakout.aboutpage.AboutPage;
@@ -18,6 +20,7 @@ public class AboutFragment extends Fragment {
       //  return inflater.inflate(R.layout.about_fragment,container,false);
         ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.about_fragment, container, false);
         String des = "Desciption text";
+        simulateDayNight(0);
         View aboutPage = new AboutPage(getActivity())
                 .isRTL(false)
                 .setImage(R.drawable.csi2)
@@ -31,6 +34,22 @@ public class AboutFragment extends Fragment {
         viewGroup.addView(aboutPage);
         return viewGroup;
     }
+    void simulateDayNight(int currentSetting) {
+        final int DAY = 0;
+        final int NIGHT = 1;
+        final int FOLLOW_SYSTEM = 3;
 
+        int currentNightMode = getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK;
+        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (currentSetting == FOLLOW_SYSTEM) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
 
 }
